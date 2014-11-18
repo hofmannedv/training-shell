6b59846141d6dfb2b27f5de9aed029f684c9b6d4
Frank Hofmann <frank.hofmann@efho.de>
2014-11-18 07:11:59 +0100

    script7.sh - README file, adding release info

--- calculations/script7.sh
+++ calculations/script7.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# -----------------------------------------------------------
+# output the sum based on the values of the script parameters
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
 # Name des aufgerufenen Skripts
 echo "Skriptname: $0"
 
