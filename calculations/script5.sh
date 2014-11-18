9aba5b03d839bb448528ee604c105781000c659d
Frank Hofmann <frank.hofmann@efho.de>
2014-11-18 07:09:01 +0100

    script5.sh - README file, adding release info

--- calculations/script5.sh
+++ calculations/script5.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+# -----------------------------------------------------------
+# add two values as given on the commandline as parameters, 
+# and output its sum
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
 # Name des aufgerufenen Skripts
 echo "Skriptname: $0"
 
@@ -15,4 +24,7 @@ betrag2=$2
 betrag3=$3
 
 # Addition (komplexe Berechnung ...)
-expr $betrag1 + $betrag2 + $betrag3
+summe=`expr $betrag1 + $betrag2 + $betrag3`
+
+# Ausgabe der Gesamtsumme
+echo "Gesamtsumme: $betrag1 + $betrag2 + $betrag3 = $summe"
