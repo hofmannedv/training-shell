971a51620ec841ec8f90400ad96c9c1fc49026a7
Frank Hofmann <frank.hofmann@efho.de>
2014-11-18 07:39:23 +0100

    script7a.sh - README file, reading the script parameters

---
+++ parameters/script7a.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# -----------------------------------------------------------
+# display the script parameters
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# Name des aufgerufenen Skripts
+echo "Skriptname: $0"
+
+# Anzahl der übermittelten Parameter
+echo "Anzahl Parameter: $#"
+
+# übermittelte Parameter
+echo "Übermittelte Parameter: $*"
+
