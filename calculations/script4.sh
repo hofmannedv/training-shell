e8bd7c57b39f6ec35f1516c6ef39fb5c4a52f39c
Frank Hofmann <frank.hofmann@efho.de>
2014-11-18 07:04:32 +0100

    script4.sh - README file, adding release info

--- calculations/script4.sh
+++ calculations/script4.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+# -----------------------------------------------------------
+# add two values, and output its sum
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
 # zwei Summanden
 betrag1=100
 betrag2=50
 
 # ausrechnen des Gesamtbetrags
-expr $betrag1 + $betrag2
+summe=`expr $betrag1 + $betrag2`
+
+# ausgeben der Summe
+echo "Summe: $betrag1 + $betrag2 = $summe"
