bb5350ee0cf0b038d09702514bf830601b3a1d22
Frank Hofmann <frank.hofmann@efho.de>
2016-04-29 16:55:51 +0200

    awk tutorial: awk-Programm als Skript

---
+++ awk/output-content-with-condition.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/awk -f
+
+# -----------------------------------------------------------
+# output these lines of the file that contain the given pattern
+#o
+# (C) 2016 Frank Hofmann, Berlin, Germany
+# with thanks to Elmar Heebs, Zürich, Switzerland
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+/2016-01-11/ {
+    print
+}
