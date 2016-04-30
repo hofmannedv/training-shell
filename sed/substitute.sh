89aec0df52d7f3f8e695ae37f017f37897543086
Frank Hofmann <frank.hofmann@efho.de>
2016-04-30 15:10:06 +0200

    sed tutorial: ausführbares sed-Skript

---
+++ sed/substitute.sh
@@ -0,0 +1,12 @@
+#!/bin/sed -f
+
+# -----------------------------------------------------------
+# substitute a search pattern by another string
+#o
+# (C) 2016 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+s/user fho/benutzer/
+
