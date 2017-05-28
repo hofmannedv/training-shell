983c2666fc9c709ab9eb24320bb19f4854386bf7
Frank Hofmann <frank.hofmann@efho.de>
2017-05-28 17:53:22 +0200

    get user name: count the running time (optimized version)

---
+++ user/count-parallel.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# -----------------------------------------------------------
+# evaluate the processing time 
+# optimized version with parallel processing and grep -F
+#
+# (C) 2017 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define loops
+loops=10000
+
+echo
+echo "basic version:"
+time {
+	for((i=$loops; i>0; i--))
+	do
+		userId=$UID
+		userName=$(whoami)
+		echo "the script runs as user $userName with UNIX id $userId"
+	done
+} | grep -F "real"
+
+echo
+echo "grep and cut:"
+time {
+	for((i=$loops; i>0; i--))
+	do
+		userId=$UID
+		userName=$(grep -F ":$UID:" /etc/passwd | cut -d: -f 1 )
+		echo "the script runs as user $userName with UNIX id $userId"
+	done
+} | grep -F "real"
+
+echo
+echo "grep and awk:"
+time {
+	for((i=$loops; i>0; i--))
+	do
+		userId=$UID
+		userName=$(grep -F ":$UID:" /etc/passwd | awk -F : '{ print $1 }' )
+		echo "the script runs as user $userName with UNIX id $userId"
+	done
+} | grep -F "real"
+
+echo
+echo "awk, only:"
+time {
+	for((i=$loops; i>0; i--))
+	do
+		userId=$UID
+		userName=$(awk -F : '/:'$userId':/ { print $1 }' /etc/passwd &)
+		echo "the script runs as user $userName with UNIX id $userId"
+	done
+} | grep -F "real"
+
+echo
+echo "perl version:"
+time {
+	for((i=$loops; i>0; i--))
+	do
+		./getuserid.pl &
+	done
+} | grep -F "real"
