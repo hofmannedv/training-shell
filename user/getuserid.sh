c7cf3d7faf453e10f0ce22a57151a38844b2d316
Frank Hofmann <frank.hofmann@efho.de>
2017-05-28 11:37:51 +0200

    get user name: improved readability

--- user/getuserid.sh
+++ user/getuserid.sh
@@ -9,16 +9,16 @@
 # -----------------------------------------------------------
 
 userId=$UID
-userName=`whoami`
+userName=$(whoami)
 
 # alternative ways to derive the user name from the id:
 # - grep and cut
-# userName=`grep ":$UID:" /etc/passwd | cut -d: -f 1`
+# userName=$(grep ":$UID:" /etc/passwd | cut -d: -f 1)
 #
 # - grep and awk
-# userName=`grep ":$UID:" /etc/passwd | awk -F : '{ print $1 }'`
+# userName=$(grep ":$UID:" /etc/passwd | awk -F : '{ print $1 }')
 #
 # - awk, only
-# userName=`awk -F : '/:'$userId':/ { print $1 }' /etc/passwd`
+# userName=$(awk -F : '/:'$userId':/ { print $1 }' /etc/passwd)
 
 echo "the script runs as user $userName with UNIX id $userId"
